9ec7544bf53ccc15c1b324a9f45ccc8147f97895
mengineer10 <48296805+mengineer10@users.noreply.github.com>
2021-05-29 06:10:24 -0700

    used robotoFont. created navbar&social-bar&header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialBarComponent } from './social-bar/social-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    SocialBarComponent
   ],
   imports: [
     BrowserModule,
