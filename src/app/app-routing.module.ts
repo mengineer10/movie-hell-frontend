b06aa1f82a4d12d284d0bc48f34464bfb5d26123
mengineer10 <48296805+mengineer10@users.noreply.github.com>
2021-06-06 02:50:49 -0700

    added movie detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieComponent } from './movie/movie.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path:'movies',component:MovieComponent},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
+  {path:'movies/detail/:movieId',component:MovieDetailComponent},
   {path:'',redirectTo:'movies',pathMatch:'full'}
 ];
 
