71ff3c462ba7c769c2a784d8d16c1b8b537a37c2
mengineer10 <48296805+mengineer10@users.noreply.github.com>
2021-07-01 03:09:16 -0700

    added register form validation

---
+++ src/app/_models/Customer.ts
@@ -0,0 +1,14 @@
+export class Customer{
+
+    constructor(){
+        this.id=0;
+        this.email="";
+        this.password="";
+        this.passwordRepeat="";
+    }
+
+    id?: number;
+    email?: string;
+    password?: string;
+    passwordRepeat?: string;
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../_models/Customer';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public formRegister: FormGroup;
+  public customer: Customer;
+
+  constructor() {
+    this.customer=new Customer();
+    this.formRegister=new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(4)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      passwordRepeat: new FormControl('', [Validators.required, Validators.minLength(4)])
+    });
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  onRegister(event: any): void{
+    //event.preventDefault();
+    alert(this.formRegister.get('email')?.touched+' '+this.formRegister.get('email')?.invalid);
+    return;
   }
 
 }
